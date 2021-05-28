3d171c54210fad823079ab5ead8d9d1e7d168fe9
imcooder <imcooder@gmail.com>
2021-05-28 22:51:30 +0800

    add ms

---
+++ ms/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,25 @@
+set-location c:
+# Useful shortcuts for traversing directories
+function cd...  { cd ..\.. }
+function cd.... { cd ..\..\.. }
+
+# Compute file hashes - useful for checking successful downloads 
+function md5    { Get-FileHash -Algorithm MD5 $args }
+function sha1   { Get-FileHash -Algorithm SHA1 $args }
+function sha256 { Get-FileHash -Algorithm SHA256 $args }
+
+# Quick shortcut to start notepad
+function n      { notepad $args }
+
+# ssh-baidu
+function ssh-baidu { ssh xuejuntao@relay01.baidu.com }
+
+# ls
+function ll		{ ls $args }
+
+# git
+function gup	{ git pull }
+function greset  { git reset --hard origin/master }
+function gpush   { git push origin "HEAD:refs/for/master" }
+function gs      { git status }
+function gb      { git branch }
